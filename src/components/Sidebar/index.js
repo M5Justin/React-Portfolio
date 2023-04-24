ba1e85aa157d0da78058956842be1c4a86cd22b9
unknown <justinmoranre@gmail.com>
2023-04-24 18:16:45 -0400

    navbar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
 import LogoS from '../../assets/images/Mwhite.png'
 import LogoSubtitle from '../../assets/images/MoranW.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faUser, faHome } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
 <div className='nav-bar'>
@@ -10,7 +12,15 @@ const Sidebar = () => (
             <img className='sub-logo' src={LogoSubtitle} alt='moran' />
     </Link>
     <nav>
-        
+        <NavLink exact='true' activeclassname='active' to='/'>
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact='true' activeclassname='active' className="about-link" to='/about'>
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact='true' activeclassname='active' className="contact-link" to='/contact'>
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
     </nav>
 
 </div>
